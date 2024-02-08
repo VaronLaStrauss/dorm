1951d217d4e6f467eaeca037d898f58f7c6cb95d
Viany Manuel <jeangmanuel@gmail.com>
2024-02-08 20:31:21 +0800

    Fixed relation type

--- src/types/relation.types.ts
+++ src/types/relation.types.ts
@@ -1,19 +1,19 @@
+import { ExtendedType, Forward, Relations, Reverse, Type } from "../classes";
+import { PredicateType, _PicklePredicates } from "../utils";
 import { ExtendedPredicates } from "./predicate.types";
-import { Type, ExtendedType, forward, reverse, Relations } from "../classes";
 import { TypeRecord } from "./type.types";
-import { _PicklePredicates, PredicateType } from "../utils";
 
 export type PickleReverse<T extends Type> = {
   [key in keyof T["predicateRecord"]]: T["predicateRecord"][key]["options"]["type"] extends PredicateType.NODE
-    ? key
-    : never;
+  ? key
+  : never;
 }[keyof T["predicateRecord"]];
 
 export type Relation<T extends Type | ExtendedType> = {
   [key in keyof Pick<
     ExtendedPredicates<T>,
     _PicklePredicates<ExtendedPredicates<T>>
-  >]: ReturnType<typeof forward> | ReturnType<typeof reverse>;
+  >]: Forward | Reverse;
 };
 
 export type RelationsRecord<TR extends TypeRecord> = {
