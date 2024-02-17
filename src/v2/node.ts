52d5b166c5fbb54698c7bb167a4dcff38a00f2e5
Viany Manuel <jeangmanuel@gmail.com>
2024-02-18 01:10:33 +0800

    Added ext predicate method

--- src/v2/node.ts
+++ src/v2/node.ts
@@ -1,6 +1,5 @@
-import { build } from "./compiler/node.builder";
+import { predToNode, type PredToNode } from "./compiler/pred-to-node";
 import type { DEdge } from "./edge";
-import type { Fragment } from "./fragment";
 import type { ExtendedPredicates } from "./predicate";
 import type { InitOpts } from "./types";
 
@@ -16,20 +15,10 @@ export class DNode<
   name extends string = string,
   NPR extends NodePredicateRecord = NodePredicateRecord
 > {
-  protected _predToType?: Record<keyof ExtendedPredicates<DNode>, DNode>;
+  protected _predToNode?: PredToNode;
+  protected readonly allowedValues = new Set<string>();
 
-  constructor(public readonly name: name, public predicates: NPR) {
-    // this.init();
-  }
-
-  // init() {
-  //   for (const predName in this.predicates) {
-  //     const pred = this.predicates[predName];
-  //     if ("opts" in pred) {
-  //     }
-  //     (pred as DEdge<EdgeInit>).nodeName = this.name;
-  //   }
-  // }
+  constructor(public readonly name: name, public predicates: NPR) {}
 
   extends<EN extends Array<DNode>>(...extendedTypes: EN) {
     return new DNodeExtended(this.name, this.predicates, extendedTypes);
@@ -39,30 +28,30 @@ export class DNode<
     return [this.name];
   }
 
-  build(
-    fragment: Fragment<this>,
-    usedVars = new Map<string, unknown>(),
-    allowedValues = new Set<string>()
-  ): string {
-    return build(this, fragment, usedVars, allowedValues);
-  }
-
-  setPredToType(nodes: DNode[]) {
-    const predToType: typeof this._predToType = {};
-    for (const node of nodes) {
-      const preds = node.predicates;
-      for (const key in preds) {
-        predToType[key] = node;
-      }
-    }
-    this._predToType = predToType;
-    return predToType;
+  setPredToNode(nodes: DNode[]) {
+    const _predToNode = predToNode(nodes);
+    this._predToNode = _predToNode;
+    return _predToNode;
   }
 
   getPredToNode() {
-    let predToType = this._predToType;
-    if (!predToType) predToType = this.setPredToType([this]);
+    let predToType = this._predToNode;
+    if (!predToType) predToType = this.setPredToNode([this]);
     return predToType;
+  }
+
+  getAllowedValues() {
+    if (this.allowedValues.size) return this.allowedValues!;
+    const _predToNode = this.getPredToNode();
+    for (const key in _predToNode) {
+      const node = _predToNode[key];
+      this.allowedValues.add(`${node.name}.${key}`);
+    }
+    return this.allowedValues!;
+  }
+
+  get extendedPredicates(): NodePredicateRecord {
+    return this.predicates;
   }
 }
 
@@ -71,6 +60,7 @@ export class DNodeExtended<
   name extends string = string,
   NPR extends NodePredicateRecord = NodePredicateRecord
 > extends DNode<name, NPR> {
+  _extendedPredicates?: NodePredicateRecord;
   constructor(name: name, predicates: NPR, public extendedNodes: EN) {
     super(name, predicates);
   }
@@ -79,17 +69,27 @@ export class DNodeExtended<
     string | number,
     DNode<string, NodePredicateRecord>
   > {
-    let predToType = this._predToType;
+    let predToType = this._predToNode;
     if (!predToType)
-      predToType = this.setPredToType([
+      predToType = this.setPredToNode([
         ...(this.extendedNodes as DNode[]),
-        this as DNode,
+        this as never,
       ]);
     return predToType;
   }
 
   override get typeNames() {
     return [this.name, ...this.extendedNodes.map((node) => node.name)];
+  }
+
+  get extendedPredicates(): NodePredicateRecord {
+    if (this._extendedPredicates) return this._extendedPredicates;
+    const extended = this.extendedNodes.reduce((acc, curr) => {
+      return { ...curr.extendedPredicates, ...acc };
+    }, {} as Record<keyof (typeof this.extendedNodes)[number], (typeof this.extendedNodes)[number]["extendedPredicates"]>);
+
+    this._extendedPredicates = { ...this.predicates, ...extended };
+    return this._extendedPredicates;
   }
 }
 
@@ -100,28 +100,38 @@ export function node<name extends string, NPR extends NodePredicateRecord>(
   return new DNode(name, predicates);
 }
 
-export function relation<DN extends DNode>(
-  rel: true | (keyof ExtendedPredicates<DN> & string)
-) {
-  return rel;
+export function reverse<
+  DN extends DNode,
+  EP extends ExtendedPredicates<DN> = ExtendedPredicates<DN>,
+  predName extends keyof EP & string = keyof EP & string
+>(predName: predName) {
+  return { predName };
 }
 
-export type Relation = ReturnType<typeof relation>;
+export type Reverse = ReturnType<typeof reverse>;
+
+export function forward() {
+  return { forward: true } as const;
+}
+
+export type Forward = ReturnType<typeof forward>;
 
 export class DPredicateNode<
   NextDN extends DNode,
+  Rel extends Reverse | Forward = Forward | Reverse,
   Opts extends InitOpts = InitOpts
 > {
   constructor(
     public readonly nextNode: NextDN,
-    public readonly relation: Relation,
+    public readonly relation: Rel,
     public readonly opts: Opts = {} as Opts
   ) {}
 }
 
 export function predicateNode<
   NextDN extends DNode,
+  Rel extends Reverse | Forward = Forward | Reverse,
   Opts extends InitOpts = InitOpts
->(node: NextDN, relation: Relation, opts: Opts = {} as Opts) {
-  return new DPredicateNode<NextDN, Opts>(node, relation, opts);
+>(node: NextDN, relation: Rel, opts: Opts = {} as Opts) {
+  return new DPredicateNode<NextDN, Rel, Opts>(node, relation, opts);
 }
