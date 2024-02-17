132292a30115db3733f63783aa9c4f30a8acf9cc
Viany Manuel <jeangmanuel@gmail.com>
2024-02-18 01:27:10 +0800

    Added export, completed 2.0

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -1,7 +1,8 @@
-// export * from "./edge";
-// export * from "./filter";
-// export * from "./fragment";
-// export * from "./mutation";
-// export * from "./node";
-// export * from "./predicate";
-// export * from "./query";
+export * from "./edge";
+export * from "./filter";
+export * from "./fragment";
+export * from "./mutation";
+export * from "./node";
+export * from "./predicate";
+export * from "./query";
+export * from "./recurse";
