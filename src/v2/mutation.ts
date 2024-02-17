3a4c151d4e88aa845b85dadc8cd58686738a9a30
Viany Manuel <jeangmanuel@gmail.com>
2024-02-18 01:05:42 +0800

    Added mutation

--- src/v2/mutation.ts
+++ src/v2/mutation.ts
@@ -1,24 +1,75 @@
+import type { PredicateType } from "..";
 import type { DEdge, InferEdge } from "./edge";
-import { User } from "./example/user";
-import type { DNode, DPredicateNode } from "./node";
+import type { DNode, DPredicateNode, Forward } from "./node";
 import type { ExtendedPredicates } from "./predicate";
-import type { Flatten, NullableType } from "./types";
+import type { Flatten, NullableType, UnionToIntersection } from "./types";
 
-export function mutate<DN extends DNode>(node: DN, fields: MutationFields<DN>) {
-  return {};
+export function mutate<DN extends DNode>(
+  node: DN,
+  fields: MutationFields<DN>
+): Record<string, unknown> {
+  const predToNode = node.getPredToNode();
+  const vars: Record<string, unknown> = {};
+  for (const fieldName in fields) {
+    const value: unknown = fields[fieldName];
+    const actualNode = predToNode[fieldName];
+
+    if (fieldName === "dtype") {
+      vars["dgraph.type"] = value;
+      continue;
+    }
+    if (fieldName === "uid") {
+      vars["uid"] = value;
+      continue;
+    }
+    if (value === undefined) {
+      continue;
+    }
+
+    const predName = `${actualNode.name}.${fieldName}`;
+    const pred = actualNode.predicates[fieldName];
+
+    if (typeof pred === "function") {
+      const nextPredNode = pred() as DPredicateNode<DNode>;
+      let values: unknown;
+
+      if (value instanceof Array) {
+        values = value.map((val) =>
+          mutate(nextPredNode.nextNode, val as never)
+        );
+      } else {
+        values = mutate(nextPredNode.nextNode, value as never);
+      }
+      vars[predName] = values;
+      continue;
+    }
+
+    vars[predName] = value;
+  }
+
+  if (!("dtype" in fields)) vars["dgraph.type"] = node.typeNames;
+  return vars;
 }
 
 export type MutationFields<
   DN extends DNode,
   EP extends ExtendedPredicates<DN> = ExtendedPredicates<DN>
 > = Flatten<
-  {
-    [key in keyof EP]?: EP[key] extends DEdge<infer Opts>
-      ? NullableType<Opts, InferEdge<Opts>> | null
-      : EP[key] extends () => DPredicateNode<infer NextDN>
-      ? MutationFields<NextDN>
-      : never;
-  } & { uid?: string; dtype?: string[] }
+  UnionToIntersection<
+    {
+      [key in keyof EP]?: EP[key] extends DEdge<infer Opts>
+        ? Opts["type"] extends PredicateType.PASSWORD
+          ? string | null | undefined
+          : NullableType<Opts, InferEdge<Opts>> | null
+        : EP[key] extends () => DPredicateNode<
+            infer NextDN,
+            infer Rel,
+            infer Opts
+          >
+        ? Rel extends Forward
+          ? NullableType<Opts, MutationFields<NextDN>> | null
+          : never
+        : never;
+    } & { uid?: string; dtype?: string[] }
+  >
 >;
-
-mutate(User, {});
