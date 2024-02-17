74b32f1fb44b551db4a86477a6df60734a1f3fb1
Viany Manuel <jeangmanuel@gmail.com>
2024-02-18 01:11:19 +0800

    Added recurse and query builders

--- src/v2/query.ts
+++ src/v2/query.ts
@@ -1,15 +1,82 @@
-import type { FilterEdge, FilterFull, RecurseOpts } from "./filter";
+import { compileDirectives, compileMainFunc, parseDqlType } from "./compiler";
+import type { FilterEdge, FilterFull } from "./filter";
 import type { Fragment, FragmentReturn } from "./fragment";
 import type { DNode } from "./node";
+import type { recurse } from "./recurse";
+import { spacing } from "./utils/spacing";
+
+type QueryItems = Record<string, ReturnType<typeof query | typeof recurse>>;
+
+export function queryBlock<QI extends QueryItems>(
+  queries: QI,
+  outsourcedVars: Record<string, unknown> = {}
+) {
+  const vars: Record<string, unknown> = {};
+  const varDec = new Set<string>();
+  const queryStrs: string[] = [];
+
+  let usedVars = new Map<string, unknown>();
+  let allowedValues = new Set<string>();
+
+  for (const queryKey in queries) {
+    const query = queries[queryKey];
+    allowedValues = new Set<string>([...allowedValues, ...query.allowedValues]);
+    usedVars = new Map<string, unknown>([...usedVars, ...query.usedVars]);
+  }
+
+  for (const queryKey in queries) {
+    const query = queries[queryKey];
+    const queryStr = query.build(queryKey, usedVars, allowedValues);
+    queryStrs.push(queryStr);
+  }
+
+  for (let [key, val] of usedVars) {
+    if (key in outsourcedVars) val = outsourcedVars[key];
+    if (!val) continue;
+    let actualVal = String(val);
+    if (val instanceof Array) actualVal = `[${actualVal}]`;
+    vars[key] = actualVal;
+    varDec.add(`${key}: ${parseDqlType(val)}`);
+  }
+
+  let query = `query q`;
+  let _varDec = Array.from(varDec);
+  if (varDec.size) query += `(${_varDec.join(", ")})`;
+  query += `{\n${queryStrs.join("\n")}\n}`;
+  return { query, varDec: _varDec, vars };
+}
 
 export function query<DN extends DNode, F extends Fragment<DN>>(
-  query: Query<DN, F>
+  _queries: Query<DN, F>
 ) {
-  return "";
+  const _space = spacing(1);
+
+  const { filter, cascade, fragOpts } = _queries;
+  const { allowedValues, usedVars } = fragOpts;
+
+  function build(
+    key: string,
+    usedVars: Map<string, unknown>,
+    allowedValues: Set<string>
+  ) {
+    const directives = compileDirectives(
+      { filter, cascade },
+      usedVars,
+      allowedValues
+    );
+
+    const mainFunc = compileMainFunc(_queries, usedVars, allowedValues);
+    let query = `${_space}${key}(${mainFunc}) ${directives}`;
+    if (fragOpts)
+      query += `{\n${fragOpts.fragmentStr ?? fragOpts.build()}\n${_space}}`;
+    return query;
+  }
+
+  return { build, allowedValues, usedVars };
 }
+
+export type QueryOpts = { mainFunc: FilterEdge } & FilterFull;
 
 export type Query<DN extends DNode, F extends Fragment<DN>> = {
   fragOpts: FragmentReturn<DN, F>;
-  mainFunc: FilterEdge;
-  recurse?: RecurseOpts;
-} & FilterFull;
+} & QueryOpts;
---
+++ src/v2/recurse.ts
@@ -0,0 +1,159 @@
+import { compileDirectives, compileMainFunc } from "./compiler";
+import { buildRecurse, compileRecurse } from "./compiler/recurse.builder";
+import type { DEdge, EdgeInit, InferEdge } from "./edge";
+import type { FilterEdge, FilterFull, RecurseOpts } from "./filter";
+import type { ExpoundPred } from "./fragment";
+import { DNode, type DPredicateNode } from "./node";
+import type {
+  ExtendedPredicates,
+  PassOpt,
+  PredOpt,
+  PredicateType,
+} from "./predicate";
+import type { Flatten, UnionToIntersection } from "./types";
+import { spacing } from "./utils/spacing";
+
+export function recurse<
+  MainDN extends DNode,
+  DNs extends DNode[],
+  RF extends RecurseFragment<DNs[number] | MainDN>
+>(_query: RecurseQuery<MainDN, DNs, RF>) {
+  const _space = spacing(1);
+  const { filter, cascade, fragOpts, recurseOpts } = _query;
+  const { allowedValues, usedVars } = fragOpts;
+
+  function build(
+    key: string,
+    usedVars: Map<string, unknown>,
+    allowedValues: Set<string>
+  ) {
+    const directives = compileDirectives(
+      { filter, cascade },
+      usedVars,
+      allowedValues
+    );
+
+    const mainFunc = compileMainFunc(_query, usedVars, allowedValues);
+    let query = `${_space}${key}(${mainFunc}) ${directives}`;
+    if (recurseOpts) query += `${compileRecurse(recurseOpts)} `;
+    if (fragOpts)
+      query += `{\n${fragOpts.fragmentStr ?? fragOpts.build()}\n${_space}}`;
+    return query;
+  }
+
+  return { build, allowedValues, usedVars };
+}
+
+export function recurseFragment<
+  MainDN extends DNode,
+  DNs extends DNode[],
+  RF extends RecurseFragment<DNs[number] | MainDN>
+>(
+  mainNode: MainDN,
+  nodes: DNs,
+  fragment: RF,
+  buildNow = true
+): RecurseFragmentReturn<MainDN, DNs, RF> {
+  const usedVars = new Map<string, unknown>();
+  const allowedValues = new Set<string>([
+    ...mainNode.getAllowedValues(),
+    ...nodes.flatMap((node) => [...node.getAllowedValues()]),
+  ]);
+
+  function build() {
+    return buildRecurse(mainNode, nodes, fragment, usedVars, allowedValues);
+  }
+
+  return {
+    build,
+    fragment,
+    type: undefined as never,
+    fragmentStr: buildNow ? build() : undefined,
+    usedVars,
+    allowedValues,
+  };
+}
+
+export type RecurseQuery<
+  MainDN extends DNode,
+  DNs extends DNode[],
+  F extends RecurseFragment<MainDN | DNs[number]>
+> = {
+  fragOpts: RecurseFragmentReturn<MainDN, DNs, F>;
+  mainFunc: FilterEdge;
+  recurseOpts?: RecurseOpts;
+} & FilterFull;
+
+export type InferRecurseFragment<
+  MainDN extends DNode,
+  DNs extends DNode[],
+  RF extends RecurseFragment<MainDN | DNs[number]>,
+  EP = UnionToIntersection<ExtendedPredicates<MainDN>>
+> = Flatten<
+  UnionToIntersection<
+    {
+      [key in keyof RF]: key extends keyof EP
+        ? EP[key] extends DEdge<EdgeInit>
+          ? ExpoundPred<
+              RF[key],
+              EP[key]["opts"],
+              key,
+              InferEdge<EP[key]["opts"]>
+            >
+          : EP[key] extends () => DPredicateNode<infer NextDN>
+          ? NextDN extends DNs[number]
+            ? RF[key] extends {
+                opts: PredOpt;
+              } & FilterFull
+              ? RF[key]["opts"]["alias"] extends string
+                ? {
+                    [k in RF[key]["opts"]["alias"]]: InferRecurseFragment<
+                      NextDN,
+                      DNs,
+                      RF
+                    >;
+                  }
+                : { [k in key]: InferRecurseFragment<NextDN, DNs, RF> }
+              : { [k in key]: InferRecurseFragment<NextDN, DNs, RF> }
+            : { [k in key]: InferRecurseFragment<NextDN, DNs, RF> }
+          : never
+        : never;
+    }[keyof RF]
+  >
+>;
+
+export type RecurseFragmentReturn<
+  MainDN extends DNode,
+  DNs extends DNode[],
+  RF extends RecurseFragment<MainDN | DNs[number]>
+> = {
+  fragment: RF;
+  build: () => string;
+  type: InferRecurseFragment<MainDN, DNs, RF>;
+  fragmentStr?: string;
+  usedVars: Map<string, unknown>;
+  allowedValues: Set<string>;
+};
+
+export type NextRecurseFragment =
+  | boolean
+  | Flatten<
+      {
+        opts?: PredOpt;
+      } & FilterFull
+    >;
+
+export type RecurseFragment<
+  CurrentDN extends DNode,
+  EP extends ExtendedPredicates<CurrentDN> = ExtendedPredicates<CurrentDN>
+> = {
+  [predName in keyof EP]?: EP[predName] extends () => infer U
+    ? U extends DPredicateNode<DNode>
+      ? NextRecurseFragment
+      : never
+    : EP[predName] extends DEdge<infer Opts>
+    ? Opts["type"] extends PredicateType.PASSWORD
+      ? PassOpt
+      : boolean | PredOpt
+    : never;
+} & Partial<{ uid: boolean | PredOpt; dtype: boolean | PredOpt }>;
