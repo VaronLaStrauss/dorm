1746ddeb624c8a1a581ab7a93449921eb0635d28
Viany Manuel <jeangmanuel@gmail.com>
2024-02-21 15:05:53 +0800

    Fixed bug where deep nodes are not built properly

--- src/v2/example/user.ts
+++ src/v2/example/user.ts
@@ -18,8 +18,8 @@ export const Employee = node("Employee", {
   audits: () => predicateNode(Audit, forward(), { asArray: true }),
   employeeCode: edge({ type: EdgeType.STRING }),
   active: edge({ type: EdgeType.FLOAT, allowedValues: UserActive }),
-});
+}).extends(Human);
 
 export const User = node("User", {
   password: edge({ type: EdgeType.PASSWORD }),
-}).extends(Human, Employee);
+}).extends(Employee);
--- src/v2/utils/extract-nodes.ts
+++ src/v2/utils/extract-nodes.ts
@@ -3,11 +3,11 @@ import type { DNode, DNodeExtended } from "../node";
 export function extractAllNodes(nodes: (DNode | DNodeExtended)[]) {
   let extracted: DNode[] = [];
   for (const node of nodes) {
+    extracted = [...extracted, node];
     if ("_extendedPredicates" in node) {
-      extracted = extracted.concat(...extractAllNodes(node.extendedNodes));
+      extracted = extracted.concat(extractAllNodes(node.extendedNodes));
       continue;
     }
-    extracted.push(node);
   }
   return extracted;
 }
