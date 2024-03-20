1c240a2c18d01de96c8e677c9af2c90e76db052e
Viany Manuel <jeangmanuel@gmail.com>
2024-03-20 17:08:13 +0800

    Removed log

--- src/v2/utils/extract-nodes.ts
+++ src/v2/utils/extract-nodes.ts
@@ -3,8 +3,6 @@ import type { DNode, DNodeExtended } from "../node";
 export function extractAllNodes(nodes: (DNode | DNodeExtended)[]) {
   let extracted: DNode[] = [];
   for (const node of nodes) {
-    console.log(node);
-
     extracted = [...extracted, node];
     if ("_extendedPredicates" in node) {
       extracted = extracted.concat(extractAllNodes(node.extendedNodes));
