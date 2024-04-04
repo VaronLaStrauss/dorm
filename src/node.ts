280512d87d161a38ccbcb89a1137810c22f4d8f1
Viany Manuel <jeangmanuel@gmail.com>
2024-04-04 22:53:47 +0800

    Used function for node extraction

--- src/node.ts
+++ src/node.ts
@@ -20,8 +20,8 @@ export class DNode<
 
   constructor(public readonly name: name, public predicates: NPR) {}
 
-  extends<EN extends Array<DNode>>(...extendedNodes: EN) {
-    return new DNodeExtended(this.name, this.predicates, () => extendedNodes);
+  extends<EN extends Array<DNode>>(extendedNodes: () => EN) {
+    return new DNodeExtended(this.name, this.predicates, extendedNodes);
   }
 
   get typeNames(): string[] {
